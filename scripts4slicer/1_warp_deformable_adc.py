cfa020a18918ca42adcbba73f9569eefb8ff486d
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2024-08-15 17:13:39 +1200

    Calculate '(in_dwi_b50)_to_(in_3d).tfm' by repeating rigid registration of aligning 'in_adc.nii' with '(in_adc)_to_(in_3d).nii'.

--- scripts4slicer/1_warp_deformable_adc.py
+++ scripts4slicer/1_warp_deformable_adc.py
@@ -48,8 +48,9 @@ for eachP in pts:
         # actually patients 025,030,031 are well aligned already according to the readme.rxt, using an identity tfm instead,
         # 033 not sure, no readme.rxt found
         # Rigid registration to in_3d: see code regFunc.py#step1_2
-        rigidReg(fixedImg=os.path.join(dataDir, eachP, 'in_3d.nii'),
-                 movingImg=os.path.join(dataDir, eachP, 'in_dwi_b50.nii'),
+        # This can work because '(in_adc)_to_(in_3d).nii' is acquired by applying '(in_dwi_b50)_to_(in_3d).tfm' to 'in_adc.nii'
+        rigidReg(fixedImg=os.path.join(dataDir, eachP, 'in_adc.nii'),
+                 movingImg=os.path.join(dataDir, eachP, '(in_adc)_to_(in_3d).nii'),
                  outImg=None,
                  outTfm=tfmFileL_transition)
     else:
